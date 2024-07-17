eab93a9f35056b431f5d426331c241252a20ec67
Jason Eagleston <jason@yuken.xyz>
2024-07-17 03:24:08 -0400

    Fixed GameState.

--- src/game_state.lua
+++ src/game_state.lua
@@ -1,15 +1,5 @@
-local game_state = {}
-
 ---@class GameState
 ---@field maps Map[]
-local GameState = {}
-
----@return GameState
-function game_state.GameState:new()
-    local o = {
-        maps = {}
-    }
-    return o
-end
-
-return game_state
+GameState = class(function(game_state)
+    game_state.maps = {}
+end)
